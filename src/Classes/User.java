6b60e507515bb97da41d8b6ca03c123d5c415c2c
heberttt <hebertp054@gmail.com>
2023-11-03 17:30:55 +0800

    created user class

---
+++ src/Classes/User.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author himagi
+ */
+abstract class User {
+    protected String id;
+    protected String fullName;
+    protected String userPassword;
+    protected String contactNum;
+    
+    public abstract void createAccount();
+    
+    
+}
