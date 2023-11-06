a98c5723f963bc60d71c8fac273eb734031405e9
heberttt <hebertp054@gmail.com>
2023-11-06 17:40:11 +0800

    updated write account (unfinished)

--- src/Classes/User.java
+++ src/Classes/User.java
@@ -4,6 +4,10 @@
  */
 package Classes;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author himagi
@@ -15,6 +19,31 @@ abstract class User implements UserInterface {
     protected String contactNum;
     
     abstract void createAccount();
+    
+    
+    protected int availableId(){
+       int biggestNum = 0;
+       try {
+        File myObj = new File("/home/himagi/javaAssignment/src/Classes/Users.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            String[] dataArr = data.split(",");
+            data = dataArr[0];
+            
+            if(biggestNum <= Integer.parseInt(data)){
+                biggestNum = Integer.parseInt(data);
+            }
+        }
+        myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        } 
+       int availableId = biggestNum + 1;
+       return availableId;
+    }
+    
     
     
 
